43cc9d1fb191e6e6ef43ece3a0083caff26c3ceb
wowvaqa <wowvaqa@gmail.com>
2017-04-10 20:38:00 +0200

    1. Dodanie okna informującego o modyfikatorach statystyk ekwipunku. 2. Dodanie do ekwipunku listy zawierającej modyfikatory.

--- core/src/com/mygdx/eoh/Equipment/Equip.java
+++ core/src/com/mygdx/eoh/Equipment/Equip.java
@@ -3,14 +3,19 @@ package com.mygdx.eoh.Equipment;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
+import com.badlogic.gdx.utils.SnapshotArray;
 import com.mygdx.eoh.assets.AssetsGameInterface;
+import com.mygdx.eoh.assets.AssetsGameScreen;
 import com.mygdx.eoh.effects.EquipModificator;
 import com.mygdx.eoh.gameClasses.GameStatus;
+import com.mygdx.eoh.gameClasses.Positioning;
 
 /**
  * Class for equip
@@ -25,17 +30,16 @@ public class Equip extends Image {
     private EquipTypes equipType;
     // Kind of equip (wooden stick, swork etc.)
     private EquipKinds equipKind;
-    // If equip is put on then TRUE else FALSE
-    private boolean putOn;
     // Image that will be displayed when dragging
     private Image dragImage;
     // Description of item.
     private String description;
-    // Equipment modificator of player mob statistics
-    private EquipModificator equipModificator;
+    // Equipment modificators of player mob statistics
+    private SnapshotArray<EquipModificator> equipModificators;
 
     private Equip(Texture texture) {
         super(texture);
+        equipModificators = new SnapshotArray<EquipModificator>();
     }
 
     /**
@@ -54,6 +58,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.WoodenStick;
                 equip.equipType = EquipTypes.Weapon;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Drewniany kij";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/woodStickIcon.png", Texture.class)
@@ -66,6 +71,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.LeatherPants;
                 equip.equipType = EquipTypes.Armor;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Skórzane spodnie";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/leatherPantsIcon.png", Texture.class)
@@ -78,6 +84,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.GoldRing;
                 equip.equipType = EquipTypes.Artifact;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Złoty pierścień";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/goldRingIcon.png", Texture.class)
@@ -90,6 +97,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.MagicStaff;
                 equip.equipType = EquipTypes.Weapon;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Magiczna laska";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/magicStaffIcon.png", Texture.class)
@@ -102,6 +110,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.LeatherArmor;
                 equip.equipType = EquipTypes.Armor;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Skórzana zbroja";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/leatherArmorIcon.png", Texture.class)
@@ -114,6 +123,7 @@ public class Equip extends Image {
                 );
                 equip.equipKind = EquipKinds.SphereOfSpeed;
                 equip.equipType = EquipTypes.Artifact;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "Kula szybkości";
                 equip.dragImage = new Image(
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/sphereOfSpeedIcon.png", Texture.class)
@@ -122,10 +132,12 @@ public class Equip extends Image {
                 break;
             default:
                 equip = new Equip(
+
                         AssetsGameInterface.getInstance().getManager().get("game/interface/equipmentIcons/noneIcon.png", Texture.class)
                 );
                 equip.equipKind = EquipKinds.None;
                 equip.equipType = EquipTypes.None;
+                equip.equipModificators.add(EquipModificator.createEquipModificator(equipKind));
                 equip.description = "none";
                 break;
         }
@@ -142,22 +154,64 @@ public class Equip extends Image {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
-                new Dialog("", (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json")) {
-                    {
-                        text(equip.getDescription());
-                        button("OK", "ok");
-                    }
-
-                    @Override
-                    protected void result(Object object) {
-                        super.result(object);
-                        if (object.equals("ok")) {
-                        }
-                    }
-                }.show(GameStatus.getInstance().getMainStage());
+                GameStatus.getInstance().getMainStage().addActor(getDescriptionWindow(equip));
             }
         });
         return equip;
+    }
+
+    private static Window getDescriptionWindow(Equip equip) {
+        final Window window = new Window("", (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.setSize(600, 600);
+        window.setModal(true);
+        window.add(new Label("" + equip.getDescription(), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"))).pad(5).colspan(2);
+
+        TextButton textButton = new TextButton("OK", (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+
+        textButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                window.remove();
+            }
+        });
+
+        window.row();
+        Label labelAtc = new Label("" + EquipModificator.getAttackModificators(equip), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.add(new Image(
+                AssetsGameScreen.getInstance().getManager().get("game/interface/attackIcon.png", Texture.class))).pad(5).size(50, 50);
+        window.add(labelAtc);
+        window.row();
+
+        window.add(new Image(
+                AssetsGameScreen.getInstance().getManager().get("game/interface/defenceIcon.png", Texture.class))).pad(5).size(50, 50);
+        Label labelDef = new Label("" + EquipModificator.getDefenceModificators(equip), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.add(labelDef);
+        window.row();
+
+        window.add(new Image(
+                AssetsGameScreen.getInstance().getManager().get("game/interface/speedIcon.png", Texture.class))).pad(5).size(50, 50);
+        Label labelSpd = new Label("" + EquipModificator.getSpeedModificators(equip), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.add(labelSpd);
+        window.row();
+
+        window.add(new Image(
+                AssetsGameScreen.getInstance().getManager().get("game/interface/powerIcon.png", Texture.class))).pad(5).size(50, 50);
+        Label labelPwr = new Label("" + EquipModificator.getPowerModificators(equip), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.add(labelPwr);
+        window.row();
+
+        window.add(new Image(
+                AssetsGameScreen.getInstance().getManager().get("game/interface/wisdomIcon.png", Texture.class))).pad(5).size(50, 50);
+        Label labelWsd = new Label("" + EquipModificator.getWisdomModificators(equip), (Skin) AssetsGameInterface.getInstance().getManager().get("styles/skin.json"));
+        window.add(labelWsd);
+        window.row();
+
+        window.add(textButton).pad(5).colspan(2).size(100, 50);
+
+        Positioning.setWindowToCenter(window);
+
+        return window;
     }
 
     /***********************************************************************************************
@@ -171,14 +225,6 @@ public class Equip extends Image {
         return equipKind;
     }
 
-    public boolean isPutOn() {
-        return putOn;
-    }
-
-    public void setPutOn(boolean putOn) {
-        this.putOn = putOn;
-    }
-
     public Image getDragImage() {
         return dragImage;
     }
@@ -187,11 +233,7 @@ public class Equip extends Image {
         return description;
     }
 
-    public EquipModificator getEquipModificator() {
-        return equipModificator;
-    }
-
-    public void setEquipModificator(EquipModificator equipModificator) {
-        this.equipModificator = equipModificator;
+    public SnapshotArray<EquipModificator> getEquipModificators() {
+        return equipModificators;
     }
 }
