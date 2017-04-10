1ce58259fb001af3d3a644e84c30455b36385405
wowvaqa <wowvaqa@gmail.com>
2017-04-10 21:15:37 +0200

    1. Dodanie modyfikatorów statystyk dla efektu.

--- core/src/com/mygdx/eoh/effects/LongEffect.java
+++ core/src/com/mygdx/eoh/effects/LongEffect.java
@@ -19,7 +19,11 @@ public class LongEffect extends AnimatedImage {
     private PlayerMob playerOwner;
     private float timeEnd;
 
-    private int attackModificator;
+    private int attackModifier;
+    private int defenceModifier;
+    private int speedModifier;
+    private int wisdomModifier;
+    private int powerModifier;
 
     LongEffect(Animation animation, boolean isLooped, LongEffects longEffects, PlayerMob playerOwner) {
         super(animation, isLooped);
@@ -53,12 +57,43 @@ public class LongEffect extends AnimatedImage {
      * GETTERS AND SETTERS
      **********************************************************************************************/
 
-    public int getAttackModificator() {
-        return attackModificator;
+    public int getAttackModifier() {
+        return attackModifier;
     }
 
-    public void setAttackModificator(int attackModificator) {
-        this.attackModificator = attackModificator;
+    public void setAttackModifier(int attackModifier) {
+        this.attackModifier = attackModifier;
     }
 
+    public int getDefenceModifier() {
+        return defenceModifier;
+    }
+
+    public void setDefenceModifier(int defenceModifier) {
+        this.defenceModifier = defenceModifier;
+    }
+
+    public int getSpeedModifier() {
+        return speedModifier;
+    }
+
+    public void setSpeedModifier(int speedModifier) {
+        this.speedModifier = speedModifier;
+    }
+
+    public int getWisdomModifier() {
+        return wisdomModifier;
+    }
+
+    public void setWisdomModifier(int wisdomModifier) {
+        this.wisdomModifier = wisdomModifier;
+    }
+
+    public int getPowerModifier() {
+        return powerModifier;
+    }
+
+    public void setPowerModifier(int powerModifier) {
+        this.powerModifier = powerModifier;
+    }
 }
