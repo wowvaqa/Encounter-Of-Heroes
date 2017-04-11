96fec440ec5d11fa9ad380d6afa04ef952d87ac0
wowvaqa <wowvaqa@gmail.com>
2017-04-11 10:56:06 +0200

    1. Klasa dzieląca ekwipunek na poziomy. 2. Klasa umożliwa losowanie ekwipunku z danego poziomu.

---
+++ core/src/com/mygdx/eoh/Equipment/EquipmentLevels.java
@@ -0,0 +1,53 @@
+package com.mygdx.eoh.Equipment;
+
+import com.badlogic.gdx.utils.SnapshotArray;
+
+import java.util.Random;
+
+/**
+ * Contains level 1 and 2 equip.
+ * Class to draw equip.
+ * Created by wowvaqa on 11.04.17.
+ */
+
+class EquipmentLevels {
+    private static final EquipmentLevels instance = new EquipmentLevels();
+
+    //All level 1 equipment
+    private SnapshotArray<EquipKinds> equipLevel1;
+    private SnapshotArray<EquipKinds> equipLevel2;
+
+    private EquipmentLevels() {
+        equipLevel1 = new SnapshotArray<EquipKinds>();
+        equipLevel2 = new SnapshotArray<EquipKinds>();
+
+        fillEquipment();
+    }
+
+    static EquipmentLevels getInstance() {
+        return instance;
+    }
+
+    private void fillEquipment() {
+        // Level 1 equip
+        equipLevel1.add(EquipKinds.WoodenStick);
+        equipLevel1.add(EquipKinds.LeatherPants);
+        equipLevel1.add(EquipKinds.GoldRing);
+
+        // Level 2 equip
+        equipLevel2.add(EquipKinds.MagicStaff);
+        equipLevel2.add(EquipKinds.LeatherArmor);
+        equipLevel2.add(EquipKinds.SphereOfSpeed);
+    }
+
+    EquipKinds drawLevel1Equip() {
+        Random rnd = new Random();
+        return equipLevel1.get(rnd.nextInt(equipLevel1.size));
+    }
+
+    EquipKinds drawLevel2Equip() {
+        Random rnd = new Random();
+        return equipLevel1.get(rnd.nextInt(equipLevel2.size));
+    }
+
+}
